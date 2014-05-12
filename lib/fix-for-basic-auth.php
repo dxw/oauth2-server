d7f5921c58eeb4a600af9aaba8c40334eb106e18
Tom Adams <tom@dxw.com>
2014-05-12 16:10:47 -0400

    Fix fix

--- lib/fix-for-basic-auth.php
+++ lib/fix-for-basic-auth.php
@@ -1,6 +1,8 @@
 <?php
 
 // league/oauth2-server attempts to be helpful and use the Basic auth username to determine the client_id
-// That is wrong
+// That won't work with staging sites
 
+unset($_SERVER['PHP_AUTH_USER']);
+unset($_SERVER['PHP_AUTH_PW']);
 unset($_SERVER['HTTP_AUTHORIZATION']);
