541a1325d3815cbb0dabb5b59efb95fceb8799d0
Tom Adams <tom@dxw.com>
2014-05-13 13:22:26 -0400

    Take client details from ACF

--- lib/model-client.php
+++ lib/model-client.php
@@ -1,23 +1,25 @@
 <?php
 
-$GLOBALS['clients'] = [
-  '123' => [
-    'client_id' => '123',
-    'client secret' => '456',
-    'redirect_uri' => 'http://abc/happy',
-    'name' => 'Happy Client'
-  ],
-];
-
 class ClientModel implements \League\OAuth2\Server\Storage\ClientInterface {
 
+  // Get client array from ACF option field
   public function getClient($clientId = null, $clientSecret = null, $redirectUri = null, $grantType = null) {
 
-    //TODO: clients should be stored in the DB and we should check that clientId/redirectUri match
+    $num_clients = (int)get_option('options_client_applications');
 
-    // echo(htmlspecialchars(json_encode([$clientId, $clientSecret, $redirectUri, $grantType]))."<br>\n");
-    if (isset($GLOBALS['clients'][$clientId])) {
-      return $GLOBALS['clients'][$clientId];
+    for ($i = 0; $i < $num_clients; $i++) {
+
+      $id = get_option(sprintf('options_client_applications_%d_client_id', $i));
+
+      if ($id === $clientId) {
+        return [
+          'client_id' => $id,
+          'client secret' => get_option(sprintf('options_client_applications_%d_client_secret', $i)),
+          'redirect_uri' => get_option(sprintf('options_client_applications_%d_redirect_uri', $i)),
+          'name' => get_option(sprintf('options_client_applications_%d_name', $i)),
+        ];
+      }
+
     }
 
     return false;
