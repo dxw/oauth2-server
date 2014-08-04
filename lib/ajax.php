9925635c01f9d8135f55423bea8a3c970ad51485
Tom Adams <tom@dxw.com>
2014-08-04 10:54:07 -0400

    Add debugging information to token endpoint if constant is set

--- lib/ajax.php
+++ lib/ajax.php
@@ -62,36 +62,65 @@ class OAuth2Server_Ajax {
   function token() {
     global $wpdb;
 
+    $debug = [];
+
+    $t1 = microtime(true);
+
     $server = new \League\OAuth2\Server\Authorization(new ClientModel, new SessionModel, new ScopeModel);
     $server->addGrantType(new \League\OAuth2\Server\Grant\AuthCode());
     $server->addGrantType(new \League\OAuth2\Server\Grant\RefreshToken());
 
     $server->getGrantType('refresh_token')->rotateRefreshTokens(true);
 
+    $debug['t1'] = microtime(true) - $t1;
+    $t2 = microtime(true);
+
     try {
       $p = $server->issueAccessToken();
     } catch (\League\OAuth2\Server\Exception\ClientException $e) {
-      // No funny business
+      $debug['t2'] = microtime(true) - $t2;
+
+      $p = ['error' => true, 'message' => 'invalid grant type'];
+
+      if (defined('OAUTH2_SERVER_DEBUG') && OAUTH2_SERVER_DEBUG === true) {
+        $p['debug'] = $debug;
+      }
+
       header('HTTP/1.1 500 Internal Server Error');
-      echo 'invalid grant type';
+      echo json_encode($p);
       die();
     }
+
+    $debug['t2'] = microtime(true) - $t2;
+    $t3 = microtime(true);
 
     // Add user data
 
     $user_id = absint($wpdb->get_var($wpdb->prepare("SELECT owner_id FROM {$wpdb->prefix}oauth2_server_sessions WHERE id IN (SELECT session_id FROM {$wpdb->prefix}oauth2_server_access_tokens WHERE access_token=%s)", $p['access_token'])));
     if ($user_id === 0) {
+      $p = ['error' => true, 'message' => 'invalid auth code'];
+
+      if (defined('OAUTH2_SERVER_DEBUG') && OAUTH2_SERVER_DEBUG === true) {
+        $p['debug'] = $debug;
+      }
+
       header('HTTP/1.1 500 Internal Server Error');
-      echo 'invalid auth code';
+      echo json_encode($p);
       die();
     }
     $user = get_user_by('id', $user_id);
+
+    $debug['t3'] = microtime(true) - $t3;
 
     $p['information'] = [
       'email' => $user->data->user_email,
       'display_name' => $user->data->display_name,
       'extra' => apply_filters('oauth2_server_extra', [], $user_id),
     ];
+
+    if (defined('OAUTH2_SERVER_DEBUG') && OAUTH2_SERVER_DEBUG === true) {
+      $p['debug'] = $debug;
+    }
 
     echo json_encode($p);
 
